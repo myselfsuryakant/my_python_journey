e84fb89c71eee206312ba968d06682ecabf7939e
Suryakant <va.suryakant@gmail.com>
2025-07-10 02:50:31 +0530

    Add new program

---
+++ 09_while_loops/while-loop.py
@@ -0,0 +1,13 @@
+# if you are asked to print digits 1 to 5, you may write it as
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# But if we want to write the same in while loop
+
+i = 1
+while i < 10:
+    print(i)
+    i = i + 1
