30172cd400a44d6886346741649c8a139c295819
Suryakant <va.suryakant@gmail.com>
2025-07-18 03:52:03 +0530

    use if-else for this question

--- 11_problem_based_on_these_topics/11_tax_calculator.py
+++ 11_problem_based_on_these_topics/11_tax_calculator.py
@@ -1,4 +1,5 @@
 '''
+Use If-Else Statamemt.
 Once upon a time there was a land – a land of milk and honey, inhabited by happy and prosperous people. The people paid taxes, of course – their happiness had limits. The most important tax, called the Personal Income Tax (PIT for short) had to be paid once a year, and was evaluated using the following rule:
 
     if the citizen's income was not higher than 85,528 thalers, the tax was equal to 18% of the income minus 556 thalers and 2 cents (this was what they called tax relief)
